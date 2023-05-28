01eb90792de1a60e095c3a17a8d951052ea7c388
ekameysha <ekameyshaaa@gmail.com>
2023-05-28 18:25:16 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+//,
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://admin:admin@cluster0.7lpp7uc.mongodb.net/test?retryWrites=true&w=majority`,
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
